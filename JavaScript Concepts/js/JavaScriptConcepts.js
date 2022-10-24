84989e6895fb98bed8122ec31b91ea544a67bd8a
Midul-islam-Russell <midulraddy121@gmail.com>
2022-10-24 12:40:27 +0600

    add

---
+++ JavaScript Concepts/js/JavaScriptConcepts.js
@@ -0,0 +1,13 @@
+/* 
+1.Variable that is not initialized will give undefined
+2. Function with no return
+3.parameter that is not passed will be undefined
+4.if return has nothing on the right side will return undefined
+5.property that doesn't 
+6.accessing array elements outside of the index range
+7.
+8.
+9.
+10
+
+*/
